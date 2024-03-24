b089caeae435dc6058f7c025c9ccc679750be7a6
nagalamadaka20p <preeti_nagalamadaka@brown.edu>
2024-03-24 00:55:25 -0400

    added more questions fix something with cervix questions later

--- pap-smear-app/src/components/Survey/Survey.jsx
+++ pap-smear-app/src/components/Survey/Survey.jsx
@@ -24,7 +24,10 @@ function Survey() {
   function handleOnTroubleshootingChange(key, val) {
     let newForm = {
       age: troubleshooting.age,
-      ageGroup: troubleshooting.ageGroup
+      hivAnswer: troubleshooting.hivAnswer,
+      cervixAnswer: troubleshooting.cervixAnswer,
+        papAnswer: troubleshooting.papAnswer,
+        documentationAnswer: troubleshooting.documentationAnswer
     };
     newForm[key] = val;
     setTroubleshooting(newForm);
@@ -49,12 +52,159 @@ function Survey() {
                 required
               ></input>
             </div>
-            {ageGroup === "under21" && (
-                <div className="question">
+            <div className="question">
                     <li>
                     Has a provider ever told you that you have HIV?
                     </li>
-                    <select > </select>
+                    <select
+                    name="hivAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.hivAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "hivAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+            {(ageGroup === "under21" && troubleshooting.hivAnswer === "no") &&(
+                <div className="question">
+                    <p>Pap not indicated</p>
+                </div>
+            )}
+            {((ageGroup === "under21" || ageGroup === "21-29")&& troubleshooting.hivAnswer === "yes") &&(
+                <div className="question">
+                    <p>HIV algorithm</p>
+                </div>
+            )}
+            {(ageGroup === "21-29" && troubleshooting.hivAnswer === "no") &&(
+                <div className="question">
+                    <li>
+                    Do you have a cervix?
+                    </li>
+                    <select
+                    name="cervixAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.cervixAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "cervixAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+            )}
+            {(ageGroup === "30-65" && troubleshooting.hivAnswer === "no") &&(
+                <div className="question">
+                    <li>
+                    Do you have a cervix? Describe hysterectomy types?
+                    </li>
+                    <select
+                    name="cervixAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.cervixAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "cervixAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+            )}
+            {troubleshooting.cervixAnswer === "no" &&(
+                <div className="question">
+                   <p>No cervix algorithm</p>
+                </div>
+            )}
+            {(troubleshooting.cervixAnswer === "yes") &&(
+                <div className="question">
+                    <li>
+                    When was your last pap smear?
+                    </li>
+                    <select
+                    name="papAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.papAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "papAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="over3"> over 3 years</option>
+                        <option value="under3"> less than 3 years or never </option>
+                    </select>
+                </div>
+            )}
+            {troubleshooting.papAnswer === "over3" &&(
+                <div className="question">
+                    <p>High risk</p>
+                </div>
+            )}
+            {troubleshooting.papAnswer === "under3" &&(
+                <div className="question">
+                     <li>
+                    Do you have documentation of your results?
+                    </li>
+                    <select
+                    name="documentationAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.documentationAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "documentationAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="yes">yes</option>
+                        <option value="no">no</option>
+                    </select>
+                </div>
+            )}
+            {troubleshooting.documentationAnswer === "yes" &&(
+                <div className="question">
+                    <p>Insert things explaining how to understand documentation</p>
+                    <li>
+                    Is the documentation normal?
+                    </li>
+                    <select
+                    name="normalDocAnswer"
+                    className="troubleshooting-dropdown"
+                    value={troubleshooting.normalDocAnswer}
+                    onChange={(e) => {
+                    handleOnTroubleshootingChange(
+                        "normalDocAnswer",
+                        e.target.value
+                    );}}>
+                        <option value="">Select an Option</option>
+                        <option value="yes">yes</option>
+                        <option value="no">no</option>
+                    </select>
+                </div>
+            )}
+            {troubleshooting.normalDocAnswer === "no" &&(
+                <div className="question">
+                    <p>Follow up with provider</p>
+                </div>
+            )}
+            {troubleshooting.normalDocAnswer === "yes" &&(
+                <div className="question">
+                    <p>Get screened 3 years after last pap smear.</p>
+                </div>
+            )}
+            {troubleshooting.documentationAnswer === "no" &&(
+                <div className="question">
+                    <p>Try to get your results; describe how to get your results. 
+                        If you do not remember where you last had a Pap smear, it would be best to see a provider</p>
                 </div>
             )}
           </ol>
